030e9f3274212a9e99486526eb11e925141b5b72
Jason Speckman <jspeckman@gmail.com>
2019-12-05 20:06:58 -0600

    Config file in local dir or /etc

--- isy_homie_start.py
+++ isy_homie_start.py
@@ -5,9 +5,13 @@ import yaml
 
 from isy_homie.bridge import Bridge
 
+try:
+    with open("/etc/isy_homie.yml", 'r') as ymlfile:
+        cfg = yaml.full_load(ymlfile)
+except FileNotFoundError:
+    with open('config.yaml', 'r') as ymlfile:
+        cfg = yaml.full_load(ymlfile)
 
-with open("isy_homie.yml", 'r') as ymlfile:
-    cfg = yaml.full_load(ymlfile)
 
 try:
     bridge = Bridge (address=cfg['isy'] ['url'], username=cfg['isy'] ['username'],password=cfg['isy'] ['password'],mqtt_settings=cfg['mqtt'])
