7a502d7f3aa574b6be7bb5ebe61420dc8de9c46e
Jason Speckman <jspeckman@gmail.com>
2019-12-05 19:49:34 -0600

    Added lock support

--- isy_homie/devices/lock.py
+++ isy_homie/devices/lock.py
@@ -3,6 +3,7 @@
 from homie.device_base import Device_Base
 from homie.node.node_base import Node_Base
 
+from homie.node.property.property_battery import Property_Battery
 from homie.node.property.property_string import Property_String
 from homie.node.property.property_enum import Property_Enum
 
@@ -14,7 +15,7 @@ class Lock (Base,Device_Base):
 
         Base.__init__ (self,isy_device)
 
-        Device_Base.__init__ (self,self.get_homie_device_id(), isy_device.name, homie_settings, mqtt_settings)
+        Device_Base.__init__ (self,self.get_homie_device_id().replace("_", "-"), isy_device.name, homie_settings, mqtt_settings)
 
         node = (Node_Base(self,'lock','Lock','lock'))
         self.add_node (node)
@@ -22,8 +23,11 @@ class Lock (Base,Device_Base):
         self.status = Property_String (node,'status','Status')
         node.add_property (self.status)
 
-        self.run = Property_Enum (node,id='lock',name='Lock',data_format='LOCK,UNLOCK',set_value = self.set_lock)
-        node.add_property (self.lock)
+        self.lock_state = Property_Enum (node,id='lockstate',name='Lock State',data_format='LOCK,UNLOCK',set_value = self.set_lock)
+        node.add_property (self.lock_state)
+
+        self.battery_level = Property_Battery (node)
+        node.add_property (self.battery_level)
 
         self.start()
 
@@ -32,13 +36,17 @@ class Lock (Base,Device_Base):
 
     def set_lock (self,value):
         if value == 'LOCK':
-            self.isy_device.set_lock (1)
+            self.isy_device.lock ()
         elif value == 'UNLOCK':
-            self.isy_device.set_lock(0)
+            self.isy_device.unlock()
             
     def property_change(self,property_,value):
-        if property_ == 'state':
+        if property_ == 'status':
             self.status.value = value
+        if property_ == 'state':
+            self.lock_state.value = value
+        if property_ == 'batterylevel':
+            self.battery_level.value = value
 
         Base.property_change (self,property_,value)
 
